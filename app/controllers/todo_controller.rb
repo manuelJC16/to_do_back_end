46a9bb3057e081c1b8078571b6c2aba6f67004d5
Manuel Carcamo <manuelchavez200292@gmail.com>
2018-11-17 20:12:06 +0000

    datebase

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -3,25 +3,9 @@ class TodoController < ApplicationController
     end
     
     def show 
-        todo_id=params[:id]
-        if todo_id=="1"
-            @todo_description = "go to the program"
-            @todo_pomodoro_estimate = 4
-        elsif todo_id=="2"
-            @todo_description = "don't forget your laptop"
-            @todo_pomodoro_estimate = 3
-        elsif todo_id=="3"
-            @todo_description = "bring the given homework"
-            @todo_pomodoro_estimate = 4
-        elsif todo_id=="4"
-            @todo_description = "bring a pen"
-            @todo_pomodoro_estimate = 4
-        elsif todo_id=="5"
-            @todo_description = "think of how great the instructor is"
-            @todo_pomodoro_estimate = 4
-        elsif todo_id=="6"
-            @todo_description = "leave the building"
-            @todo_pomodoro_estimate = 4
-        end
+@todo = Todo.find_by_id(params[:id])
+
+
+            
     end
 end
