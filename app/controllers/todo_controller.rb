384195096a7064d2fcc0f6934c17b098a20c500d
Manuel Carcamo <manuelchavez200292@gmail.com>
2018-11-17 17:31:06 +0000

    update

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -10,6 +10,18 @@ class TodoController < ApplicationController
         elsif todo_id=="2"
             @todo_description = "clean room"
             @todo_pomodoro_estimate = 3
+        elsif todo_id=="3"
+            @todo_description = ""
+            @todo_pomodoro_estimate = 4
+        elsif todo_id=="4"
+            @todo_description = ""
+            @todo_pomodoro_estimate = 4
+        elsif todo_id=="5"
+            @todo_description = ""
+            @todo_pomodoro_estimate = 4
+        elsif todo_id=="6"
+            @todo_description = ""
+            @todo_pomodoro_estimate = 4
         end
     end
 end
