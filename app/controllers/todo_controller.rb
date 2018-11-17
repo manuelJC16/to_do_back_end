a382c7472768d81152301afb6b7d140ab9b86c4b
Manuel Carcamo <manuelchavez200292@gmail.com>
2018-11-17 19:19:42 +0000

    update

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -8,19 +8,19 @@ class TodoController < ApplicationController
             @todo_description = "go to the program"
             @todo_pomodoro_estimate = 4
         elsif todo_id=="2"
-            @todo_description = "clean room"
+            @todo_description = "don't forget your laptop"
             @todo_pomodoro_estimate = 3
         elsif todo_id=="3"
-            @todo_description = ""
+            @todo_description = "bring the given homework"
             @todo_pomodoro_estimate = 4
         elsif todo_id=="4"
-            @todo_description = ""
+            @todo_description = "bring a pen"
             @todo_pomodoro_estimate = 4
         elsif todo_id=="5"
-            @todo_description = ""
+            @todo_description = "think of how great the instructor is"
             @todo_pomodoro_estimate = 4
         elsif todo_id=="6"
-            @todo_description = ""
+            @todo_description = "leave the building"
             @todo_pomodoro_estimate = 4
         end
     end
