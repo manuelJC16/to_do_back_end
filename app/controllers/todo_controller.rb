088bd4cecf66a54cb59ee2cc9b066763f21a6ab0
Manuel Carcamo <manuelchavez200292@gmail.com>
2018-11-17 17:20:31 +0000

    update

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,9 +1,15 @@
 class TodoController < ApplicationController
     def index
     end
-    def show
-        @todo_description = "go to the program"
-@todo_pomodoro_estimate = 4 
-
+    
+    def show 
+        todo_id=params[:id]
+        if todo_id=="1"
+            @todo_description = "go to the program"
+            @todo_pomodoro_estimate = 4
+        elsif todo_id=="2"
+            @todo_description = "clean room"
+            @todo_pomodoro_estimate = 3
+        end
     end
 end
